55bceebc2d3c461a3032c1fda6cdd600747669cc
배남석 <76683388+NamSeok-Bae@users.noreply.github.com>
2023-02-21 23:05:48 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.06 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.swift
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.swift
@@ -3,7 +3,5 @@ import Foundation
 func solution(_ lottos:[Int], _ win_nums:[Int]) -> [Int] {
     var intersection = Set(lottos).intersection(win_nums)
 
-    let first = 7 - (intersection.count + lottos.filter { $0 == 0 }.count)
-    let second = 7 - intersection.count
-    return [first >= 6 ? 6 : first , second >= 6 ? 6 : second]
+    return [min(7 - (intersection.count + lottos.filter { $0 == 0 }.count), 6), min(7 - intersection.count, 6)]
 }
