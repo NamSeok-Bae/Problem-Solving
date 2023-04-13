2d2869beaef2948f3c3aef1e2341daef57de539f
배남석 <76683388+NamSeok-Bae@users.noreply.github.com>
2023-04-14 01:49:20 +0900

    [Gold IV] Title: 약수의 합, Time: 100 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Gold/17425. 약수의 합/약수의 합.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int t,n;
+int sum;
+long long dp[1000001];
+long long arr[1000001];
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin>>t;
+    
+    for (int i=1; i<=1000000;i++) {
+        for(int j=i;j<=1000000;j+=i) {
+            dp[j] += i;
+        }
+    }
+    
+    for(int i=2;i<=1000000;i++) {
+        dp[i] += dp[i-1];
+    }
+    
+    while(t--) {
+        cin>>n;
+        cout<<dp[n]<<"\n";
+    }
+}
